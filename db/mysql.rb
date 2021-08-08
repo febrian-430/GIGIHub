2ca0551556bb55aad8ec930ccc5ca46059f81218
febrian <febrian430@gmail.com>
2021-08-08 14:20:16 +0700

    added MySQLDB#transaction
    to wrap a query around a transaction with the same client
    
    Signed-off-by: febrian <febrian430@gmail.com>

--- db/mysql.rb
+++ db/mysql.rb
@@ -2,15 +2,35 @@ require 'mysql2'
 
 class MySQLDB
     @@singleton_client = nil
+
     def self.client
         if !@@singleton_client
-            @@singleton_client = Mysql2::Client.new(
-                :host => ENV["HOST"],
-                :username => ENV["DB_USER_NAME"],
-                :password => ENV["DB_USER_PASSWORD"],
-                :database => ENV["DB_NAME"]
-            )
+            @@singleton_client = new_client
         end
         @@singleton_client
+    end
+
+    def self.new_client
+        client=Mysql2::Client.new(
+            :host => ENV["HOST"],
+            :username => ENV["DB_USER_NAME"],
+            :password => ENV["DB_USER_PASSWORD"],
+            :database => ENV["DB_NAME"]
+        )
+        client
+    end
+
+    def self.transaction(&block)
+        transaction_client = new_client
+        raise false unless block_given?
+        begin
+            transaction_client.query("START TRANSACTION")
+            yield(transaction_client)
+            transaction_client.query("COMMIT")
+            return true
+        rescue => exception
+            transaction_client.query("ROLLBACK")
+            return false
+        end
     end
 end
