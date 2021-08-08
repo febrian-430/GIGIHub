b2b09a6bf28f1073e90157748539da5331bb9c9f
febrian <febrian430@gmail.com>
2021-08-08 19:41:51 +0700

    added unit test for PostController#show_by_id
    Signed-off-by: febrian <febrian430@gmail.com>

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -38,4 +38,26 @@ describe PostController do
             end
         end
     end
+
+    describe "#show_by_id" do
+        context "when id doesnt exist" do
+            it "returns 404" do
+                allow(Post).to receive(:by_id).and_return nil
+
+                response = PostController.show_by_id({"id" => -1})
+                expect(response[:status]).to eq(404)
+            end
+        end
+
+        context "when id doesnt exist" do
+            it "returns 200" do
+                post_dbl = double("post")
+                allow(Post).to receive(:by_id).and_return post_dbl
+
+                response = PostController.show_by_id({"id" => -1})
+                expect(response[:status]).to eq(200)
+                expect(response[:body]).not_to be_nil
+            end
+        end
+    end
 end
