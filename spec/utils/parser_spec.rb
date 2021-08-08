af51f645725fa4e95ca7bd3d0f739d15576fbe7c
febrian <febrian430@gmail.com>
2021-08-08 13:56:59 +0700

    refactored Parser#words_begin_with
    to return only distinct values
    
    Signed-off-by: febrian <febrian430@gmail.com>

--- spec/utils/parser_spec.rb
+++ spec/utils/parser_spec.rb
@@ -12,6 +12,44 @@ describe Parser do
                     expect(words).to eq(expected)
                 end
             end
+
+            context "with the entire text is a hashtag" do
+                it "returns array with one element" do
+                    text = "#generasi_gigih"
+                    expected = ["generasi_gigih"]
+                    words = Parser.words_begin_with('#', text)
+                    expect(words).to eq(expected)
+                    expect(words.length).to eq(1)
+
+                end
+            end
+
+            context "with text without a word beginning with #" do
+                it "returns empty array" do
+                    text = "generasi_gigih"
+                    expected = []
+                    words = Parser.words_begin_with('#', text)
+                    expect(words).to eq(expected)
+                end
+            end
+
+            context "empty text" do
+                it "returns empty array" do
+                    text = ""
+                    expected = []
+                    words = Parser.words_begin_with('#', text)
+                    expect(words).to eq(expected)
+                end
+            end
+
+            context "with text containing duplicate" do
+                it "returns array only containing distinct values" do
+                    text = "#gigih #gigih #generasi"
+                    expected = ["gigih", "generasi"]
+                    words = Parser.words_begin_with('#', text)
+                    expect(words).to eq(expected)
+                end
+            end
         end
     end
 end
--- utils/parser.rb
+++ utils/parser.rb
@@ -1,9 +1,15 @@
+require 'set'
+
 class Parser
     def self.words_begin_with(marker, str)
+        distinct = Set[]
         words = []
         str.split(' ').each do |word|
             without_tag = word[1..(word.length-1)]
-            words << without_tag if word.start_with?(marker)
+            if word.start_with?(marker) && !distinct.include?(without_tag)
+                words << without_tag 
+                distinct << without_tag
+            end
         end
         words
     end
