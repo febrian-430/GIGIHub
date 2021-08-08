4acfa73e16d0a34ac832f00fa4519738f65d0c14
febrian <febrian430@gmail.com>
2021-08-08 11:23:12 +0700

    refactored JSONable class
    to return attributes without leading '@' as the hash key

--- utils/JSONable.rb
+++ utils/JSONable.rb
@@ -1,15 +1,18 @@
 class JSONable
     def to_json(options = {})
         hash = {}
-        self.instance_variables.each do |var|
-            hash[var] = self.instance_variable_get var
+        self.instance_variables.each do |var_symbol|
+            attribute = var_symbol.to_s
+            attribute = attribute[1..(attribute.length-1)]
+
+            hash[attribute] = self.instance_variable_get var_symbol
         end
         hash.to_json
     end
     
-    def from_json! string
-        JSON.load(string).each do |var, val|
-            self.instance_variable_set var, val
-        end
-    end
+    # def from_json! string
+    #     JSON.load(string).each do |var, val|
+    #         self.public_send var, val
+    #     end
+    # end
 end
