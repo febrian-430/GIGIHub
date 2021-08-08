dbabbe686ab4b34cc82b4dc9d69419e4cafdfc25
febrian <febrian430@gmail.com>
2021-08-08 11:59:32 +0700

    added new parameter to user#save?
    to return false when user_id is nil and the user doesnt exist in the database
    
    Signed-off-by: febrian <febrian430@gmail.com>

--- models/post.rb
+++ models/post.rb
@@ -4,7 +4,7 @@ require './utils/JSONable'
 class Post < JSONable
 
     def initialize(params)
-        @id = params["id"]
+        @id = nil
         @body = params["body"]
         @created_at = params["created_at"]
         @updated_at = params["updated_at"]
@@ -17,5 +17,9 @@ class Post < JSONable
 
     def save?
         return false if @body.nil? || @body.empty?
+        return false if @user_id.nil?
+        @user = User.by_id(@user_id)
+        return false if @user.nil?
+        true
     end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,7 @@
 require 'rspec'
 require './models/post'
+require './models/user'
+
 
 describe Post do
     describe "validation before database manipulation" do
@@ -8,6 +10,31 @@ describe Post do
                 it "returns false" do
                     post = Post.new({})
                     expect(post.save?).to eq(false)
+                end
+            end
+
+            context "when the user id is nil" do
+                it "returns false" do
+                    post = Post.new({"user_id" => nil})
+                    expect(post.save?).to eq(false)
+                end
+            end
+
+            context "when the user id doesnt exist" do
+                it "returns false" do
+                    post = Post.new({"user_id" => -1})
+                    
+                    allow(User).to receive(:by_id).and_return(nil)
+                    expect(post.save?).to eq(false)
+                end
+            end
+
+            context "when has body and valid user id" do
+                it "returns true" do
+                    post = Post.new({
+                        "body"=> "abcdefg",
+                        "user_id" => 50
+                    })
                 end
             end
         end
