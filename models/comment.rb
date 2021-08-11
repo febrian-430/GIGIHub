1c4e89992e4f7f6a65caeab7e58504b1482a8e1e
febrian <febrian430@gmail.com>
2021-08-11 11:53:19 +0700

    fixed syntax error, and refactored comment#save? unit test
    Signed-off-by: febrian <febrian430@gmail.com>

--- models/comment.rb
+++ models/comment.rb
@@ -8,7 +8,7 @@ class Comment < JSONable
     attr_accessor :id, :body, :post_id, :user_id, :created_at, :updated_at, :post, :user
     def initialize(params)
         @id = params["id"]
-        @body = params["id"]
+        @body = params["body"]
         @post_id = params["post_id"]
         @user_id = params["user_id"]
         @created_at = params["created_at"]
@@ -16,11 +16,13 @@ class Comment < JSONable
 
         @post = nil
         @user = nil
+        puts self.inspect
     end
 
     def save?
         return false if @body.nil? || @body.empty?
-        true
+        # return false if 
+        return true
     end
 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,12 +4,17 @@ require './models/comment'
 describe Comment do
     describe "validation" do
         before(:each) do
-            
+            @params = {
+                "user_id" => "1".to_i,
+                "body" => "abc",
+                "post_id" => "2".to_i
+            }
         end
         describe "#save?" do
             context "when nil comment body" do
                 it "returns false" do
-                    comment = Comment.new({})
+                    @params["body"] = nil
+                    comment = Comment.new(@params)
 
                     expect(comment.save?).to be_falsey
                 end
@@ -17,11 +22,18 @@ describe Comment do
 
             context "when empty comment body" do
                 it "returns false" do
-                    comment = Comment.new({
-                        "body": ""
-                    })
+                    @params["body"] = ""
+                    comment = Comment.new(@params)
 
                     expect(comment.save?).to be_falsey
+                end
+            end
+
+            context "when body is not nil" do
+                it "returns true" do
+                    comment = Comment.new(@params)
+
+                    expect(comment.save?).to be true
                 end
             end
         end
