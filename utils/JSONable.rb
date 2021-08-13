5dd1ac7bbc622ee01875bdd9bd9423bc3ffabe13
febrian <febrian430@gmail.com>
2021-08-13 21:34:47 +0700

    modified JSONable class to only showable fields

--- utils/JSONable.rb
+++ utils/JSONable.rb
@@ -1,12 +1,13 @@
 class JSONable
     def to_json(options = {})
         hash = {}
+        showable = self.instance_variable_get :@showable_variables
+        puts showable.inspect
         self.instance_variables.each do |var_symbol|
             attribute = var_symbol.to_s
             attribute = attribute[1..(attribute.length-1)]
-
-            hash[attribute] = self.instance_variable_get var_symbol
-        end
+            hash[attribute] = self.instance_variable_get var_symbol if showable.include? attribute
+        end 
         hash.to_json
     end
     
