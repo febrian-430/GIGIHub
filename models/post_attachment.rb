ac6715c89c08813e47aed513de32553bb399da8c
febrian <febrian430@gmail.com>
2021-08-14 13:16:39 +0700

    created abstract class Attachment, class PostAttachment and #attach_to #by_post
    class PostAttachment inherits from Attachment with its methods (attach_to, and store_files)
    
    current implementation of store_files uses StorageManager class to save files
    
    attach_to method saves the files by calling store_files and insert the filename, and mimetype to database
    
    by_post method returns attachments of a post (given an id, if it doesnt exist raises NotFoundError)
    
    Signed-off-by: febrian <febrian430@gmail.com>

--- models/post_attachment.rb
+++ models/post_attachment.rb
@@ -1,8 +1,47 @@
-require './utils/JSONable'
+require './models/attachment'
+require './exceptions/type_mismatch'
+require './db/mysql'
 
-class PostAttachment < JSONable
-    def initialize
-        @id = params["id"]
-        @filename = params["filename"]
+class PostAttachment < Attachment
+
+    def initialize(params)
+        super(params)
+        @showable_variables = ["id", "filename", "mimetype"]
+        @post_id = params["post_id"]
     end
+
+    def self.attach_to(obj, files = [])
+        raise(TypeError, "expected Post instance") unless obj.instance_of? Post
+        return 0 if files.empty?
+        client = MySQLDB.client
+        filenames = store_files(files)
+        query = "INSERT INTO post_attachments(post_id, filename, mimetype) VALUES"
+        
+        insert_elements = files.map { |file| "(#{obj.id}, '#{file["filename"]}','#{file["mimetype"]}')" }
+        query += insert_elements.join(',')
+        client.query(query)
+        return client.affected_rows
+    end
+
+    def self.by_post(post_id)
+        post = Post.by_id_exists(post_id)
+        raise NotFoundError unless post
+
+        client = MySQLDB.client
+        raw = client.query("SELECT patt.* FROM post_attachments patt JOIN posts p ON patt.post_id = p.id 
+            WHERE p.id = #{post_id}")
+        attachments = bind(raw)
+        return attachments
+    end
+
+    #private
+    def self.bind(data)
+        attachments = []
+        data.each do |row| 
+            attachment = PostAttachment.new(row)
+            attachments << attachment
+        end
+        attachments
+    end
+    private_class_method :bind
 end
---
+++ spec/models/post_attachment_spec.rb
@@ -0,0 +1,66 @@
+require 'rspec'
+require 'tempfile'
+require './models/post_attachment'
+require './models/post'
+require './utils/storage_manager'
+require './db/mysql'
+require './exceptions/type_mismatch'
+require './exceptions/not_found'
+
+
+describe PostAttachment do
+    describe "#attach_to" do
+        context "when not given an object that is not a Post" do
+            it "raises TypeMismatchError" do
+                expect { PostAttachment.attach_to({}, [])}.to raise_error(TypeError)
+            end
+        end
+
+        context "when given empty array of files" do
+            it "returns 0" do
+                post = Post.new({})
+                # allow(post).to receive(:instance_of?).and_return(true)
+
+                expect(PostAttachment.attach_to(post, [])).to eq(0)
+            end
+        end
+
+        context "when given a post object and array of files" do
+            it "returns the number of files saved" do
+                post = Post.new({})
+                files = [{"filename" => "test", "mimetype" => "image/png", "file" => Tempfile.new('test')}]
+                mock_db = double("database")
+
+                allow(MySQLDB).to receive(:client).and_return(mock_db)
+   
+                allow(PostAttachment).to receive(:store_files).with(files)
+                allow(mock_db).to receive(:affected_rows).and_return(files.length)
+                
+                expect(mock_db).to receive(:query)
+                expect(PostAttachment.attach_to(post, files)).to eq(files.length)
+            end
+        end
+    end
+
+    describe "#by_post" do
+        context "when post_id doesnt exist in database" do
+            it "raises NotFoundError" do
+                allow(Post).to receive(:by_id_exists).and_return(false)
+                expect{PostAttachment.by_post(-1)}.to raise_error(NotFoundError)
+            end
+        end
+
+        context "when post_id exists in database" do
+            it "returns post attachments" do
+                mock_db = double("database")
+                result_dbl = double("query result")
+                allow(Post).to receive(:by_id_exists).and_return(true)
+                allow(MySQLDB).to receive(:client).and_return(mock_db)
+                allow(mock_db).to receive(:query).and_return(result_dbl)
+
+                expect(PostAttachment).to receive(:bind).with(result_dbl)
+                PostAttachment.by_post(1)
+            end
+        end
+    end
+end
