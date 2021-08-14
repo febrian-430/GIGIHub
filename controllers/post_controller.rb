4fbb241bfc6a4174f6c1732e89bfa95b2233e88b
febrian <febrian430@gmail.com>
2021-08-14 14:35:45 +0700

    PostController#create now accepts attachments parameter as fileupload

--- controllers/post_controller.rb
+++ controllers/post_controller.rb
@@ -6,15 +6,29 @@ require './utils/response'
 class PostController
 
     def self.create(params)
+        attachments = []
+        if params["attachments"]
+            attachments = params["attachments"].map {
+                |data|
+                {
+                    "filename" => data["filename"],
+                    "mimetype" => data["type"],
+                    "file" => data["tempfile"]
+                }
+            } 
+        end
+
         post = Post.new({
             "user_id" => params["user_id"].to_i,
-            "body" => params["body"]
+            "body" => params["body"],
+            "attachments" => attachments
         })
 
         user = User.by_id(post.user_id)
         return DefaultResponse.not_found unless user
         
         return DefaultResponse.internal_server_error unless post.save
+
 
         response = DefaultResponse.created
         response[:body] = {
